cb9fe74699e25c96f1a9a199784d15494a7bdcfc
Rumesh Babu R <rumesh-9634@rumesh-9634.csez.zohocorpin.com>
2026-07-30 13:23:29 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#050505",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Inter",
+      }}
+    >
+      <h1>Rumesh Babu Portfolio 🚀</h1>
+    </main>
+  );
+}
